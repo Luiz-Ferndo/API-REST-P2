9cad20468dba8f6330514ad8b06d31997c046de9
Luiz <lfmenezes22@gmail.com>
2025-06-03 20:20:50 -0300

    FIX: Add autenticação no createUser

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,12 +5,13 @@ const authMiddleware = require('../middlewares/auth');
 
 //Rotas Publicas
 router.post('/login', UserController.login);
-router.post('/users', UserController.createUser);
+
 
 //Rotas Privadas
 router.get('/users', authMiddleware, UserController.getAllUsers);
 router.get('/users/:id', authMiddleware, UserController.getUserById);
 router.put('/users/:id', authMiddleware, UserController.updateUser);
 router.delete('/users/:id', authMiddleware, UserController.deleteUser);
+router.post('/users', authMiddleware, UserController.createUser);
 
 module.exports = router;
